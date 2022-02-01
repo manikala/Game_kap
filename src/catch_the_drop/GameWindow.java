434927a5ce7a7b5f8aa3b67d76b286aa4d850ea3
Valeriy <manikala777@rambler.ru>
2022-02-01 11:30:32 +0300

    Если капля покинула поле, game over выводится на экран

--- src/catch_the_drop/GameWindow.java
+++ src/catch_the_drop/GameWindow.java
@@ -40,7 +40,7 @@ public class GameWindow extends JFrame {
         drop_top = drop_top + drop_v * delta_time;
         g.drawImage(background, 0, 0, null);
         g.drawImage(drop, (int) drop_left, (int) drop_top, null);
-//        g.drawImage(game_over, 280, 120, null);
+        if (drop_top > game_window.getHeight()) g.drawImage(game_over, 280, 120, null);
 
     }
 
